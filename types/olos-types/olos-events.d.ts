675a47ab05eb56be5e49b79e2523a7f257ba2458
Arnaldo da Costa e Castro Neto <arnnet@intra.olos.com.br>
2022-11-23 14:31:10 -0300

    Giovanni changes

--- types/olos-types/olos-events.d.ts
+++
@@ -1,9 +0,0 @@
-export interface OlosEvent {
-  eventId: string;
-  agentId: string;
-  eventType: string;
-  campaignId: number;
-  channelType: string;
-  channel?: string;
-  timestamp: number;
-}
--- types/olos-types/test/olos-types-ts-tests.ts
+++
@@ -1,5 +0,0 @@
-import  VoiceEvent = require("../voice-events")
-import  TextEvent = require("../text-events")
-
-const eventVoice: VoiceEvent.LoginCCM;
-const eventText: TextEvent.LoginResponse;
