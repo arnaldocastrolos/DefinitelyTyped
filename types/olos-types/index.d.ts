93dbb50976f8ebf75f7285f62ee705ac41daaa62
Arnaldo da Costa e Castro Neto <arnnet@intra.olos.com.br>
2022-11-23 12:28:27 -0300

    Something happen and I dont know what is

--- types/olos-types/index.d.ts
+++ types/olos-types/index.d.ts
@@ -1,3 +1,10 @@
+// Type definitions for agenthub-client 1.2
+// Project: https://github.com/OlosLive/agenthub/tree/main/assets/js/agenthub-client
+// Definitions by: Valdeci Barbosa <https://github.com/valdecibarbosa>
+// Definitions: https://github.com/OlosLive/olos-types/tree/main/agent-events
+// TypeScript Version: 2.7
+
 export * from "./voice-events";
 export * from "./text-events";
 export * from "./olos-events";
+//contain alias because there are events with the same name
